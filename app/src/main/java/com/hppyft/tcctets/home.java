e052d6e3578f0dc3235306044532ba78d3c25b3b
unknown <leo@wiitec.com.br>
2018-07-20 20:35:36 -0300

    hehexd

--- app/src/main/java/com/hppyft/tcctets/home.java
+++ app/src/main/java/com/hppyft/tcctets/home.java
@@ -9,5 +9,6 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        System.out.println(" soares e jan");
     }
 }
